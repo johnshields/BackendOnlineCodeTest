7afbf4b8ccf0a13b778e0069d51d706043f6ec62
johnshields <johndshields14@gmail.com>
2025-07-15 17:56:04 +1000

    StringFormatter.cs changes: - Simplified types with "var". - Renamed qry to builder for readability.

--- StringFormatter.cs
+++ StringFormatter.cs
@@ -18,22 +18,24 @@ You may make any improvements you see fit, for example:
  * Removed {} from namespace to update to C# 10 standards.
  * Fixed typo in ToCommaSepatatedList to ToCommaSeparatedList.
  * Made class static to prevent instantiation.
+ * Simplified types with "var".
+ * Renamed qry to builder for readability.
  */
 public static class StringFormatter
 {
     //Code to improve
     public static string ToCommaSeparatedList(string[] items, string quote)
     {
-        StringBuilder qry = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
+        var builder = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
 
         if (items.Length > 1)
         {
-            for (int i = 1; i < items.Length; i++)
+            for (var i = 1; i < items.Length; i++)
             {
-                qry.Append(string.Format(", {0}{1}{0}", quote, items[i]));
+                builder.Append(string.Format(", {0}{1}{0}", quote, items[i]));
             }
         }
 
-        return qry.ToString();
+        return builder.ToString();
     }
 }
