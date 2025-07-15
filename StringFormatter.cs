b96b7b6691037960869427e51f360c804b1917ac
johnshields <johndshields14@gmail.com>
2025-07-15 17:44:02 +1000

    Added comment block for changes being made to StringFormatter.cs

--- StringFormatter.cs
+++ StringFormatter.cs
@@ -1,34 +1,39 @@
 ﻿using System.Text;
 
-namespace BackendOnlineCodeTest
+namespace BackendOnlineCodeTest;
+
+/*
+Improve a block of code as you see fit in C#.
+You may make any improvements you see fit, for example:
+  - Cleaning up code
+  - Removing redundancy
+  - Refactoring / simplifying
+  - Fixing typos
+  - Any other light-weight optimisation
+*/
+
+/*
+ * Changes:
+ * Added a Program class for testing outputs.
+ * Removed {} from namespace to update to C# 10 standards.
+ * Fixed typo in ToCommaSepatatedList to ToCommaSeparatedList.
+ * Made class static to prevent instantiation.
+ */
+public static class StringFormatter
 {
-
-    /*
-    Improve a block of code as you see fit in C#.
-    You may make any improvements you see fit, for example:
-      - Cleaning up code
-      - Removing redundancy
-      - Refactoring / simplifying
-      - Fixing typos
-      - Any other light-weight optimisation
-    */
-    public static class StringFormatter
+    //Code to improve
+    public static string ToCommaSeparatedList(string[] items, string quote)
     {
+        StringBuilder qry = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
 
-        //Code to improve
-        public static string ToCommaSeparatedList(string[] items, string quote)
+        if (items.Length > 1)
         {
-            StringBuilder qry = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
-
-            if (items.Length > 1)
+            for (int i = 1; i < items.Length; i++)
             {
-                for (int i = 1; i < items.Length; i++)
-                {
-                    qry.Append(string.Format(", {0}{1}{0}", quote, items[i]));
-                }
+                qry.Append(string.Format(", {0}{1}{0}", quote, items[i]));
             }
-
-            return qry.ToString();
         }
+
+        return qry.ToString();
     }
 }
