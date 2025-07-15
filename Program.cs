693d93e2527f084d1319f1a37597b638de1d668a
johnshields <johndshields14@gmail.com>
2025-07-15 17:27:57 +1000

    Added basic input test for StringFormatter in Program.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ public static class Program
 {
     public static void Main()
     {
+        // basic input test for StringFormatter
         string[] items = ["item1", "item2", "item3"];
         var results = StringFormatter.ToCommaSeparatedList(items, "\"");
         Console.WriteLine(results);
